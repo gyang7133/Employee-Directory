d225d5196729e47d72511a1475cc97024dbca070
Genie Yang <genieyang93@gmail.com>
2018-10-30 01:07:30 -0400

    Cleaning up HTML and editing app.js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,137 @@
-/*(function()
+// const resetInputs = function()
+// {
+//   $('#inputRow').addClass('d-none');
+//   $('#nameInput').addClass('d-none');
+//   $('#officeInput').addClass('d-none');
+//   $('#phoneInput').addClass('d-none');
+//   $('#addButton').addClass('d-none');
+//   $('#deleteButton').addClass('d-none');
+//   $('#searchButton').addClass('d-none');
+//   $('#editButton').addClass('d-none');
+//   $('#showContent').addClass('d-none');
+//   $('#name').val('');
+//   $('#officeNumber').val('');
+//   $('#phoneNumber').val('');
+// }
+// resetInputs();
+
+const showAdd = function() 
 {
-    $('#about').hide();
-})();
+  resetInputs();
+  $('#name').removeClass('d-none');
+  $('#officeNumber').removeClass('d-none');
+  $('#phoneNumber').removeClass('d-none');
+  $('#addButton').removeClass('d-none');
+  $('#inputNav').removeClass('d-none');
+}
 
-const showHome = function() 
+const showSearch = function()
+ {
+  resetInputs();
+  $('#name').removeClass('d-none');
+  $('#searchButton').removeClass('d-none');
+  $('#inputNav').removeClass('d-none');
+}
+
+const showContent = function() 
 {
-    $('#homepage').show();
-    $('#about').hide();
-};
+  resetInputs();
+  render();
+  $('#showContent').removeClass('d-none');
+}
 
-
-const showAbout = function() 
+const showDelete = function() 
 {
-    $('#homepage').hide();
-    $('#about').show();
-};
+  resetInputs();
+  $('#name').removeClass('d-none');
+  $('#deleteButton').removeClass('d-none');
+  $('#inputNav').removeClass('d-none');
+}
 
-$('#home').on('click', showHome);
-$('#about-us').on('click', showAbout);
-*/
-
+const showUpdate = function() {
+  resetInputs();
+  $('#name').removeClass('d-none');
+  $('#officeNumber').removeClass('d-none');
+  $('#phoneNumber').removeClass('d-none');
+  $('#editButton').removeClass('d-none');
+  $('#inputNav').removeClass('d-none');
+}
 
 // 1. Render all of the names in `employeeList` individually in paragraph tags to the div with the class `content`.
-const render = function() {
+
+const theCard = function (name, officeNum, phoneNum) 
+{
+    return ` <br> 
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <div class="card-text"> 
+                <p>${name}</p>
+                <p>${officeNum}</p>
+                <p>${phoneNum}</p>
+            </div>
+        </div>
+    </div>
+    <br>`;
+}
+
+const render = function() 
+{
     $('.content').empty();
-  
-    for( let i = 0; i < employeeList.length; i++ ) {
-      $('.content').append(`<div class="card">`);
-      $('.content').append(`<p>${employeeList[i].name}</p>`);
-      $('.content').append(`<p>${employeeList[i].officeNum}</p>`);
-      $('.content').append(`<p>${employeeList[i].phoneNum}</p>`);
-      $('.content').append(`</div>`);
+
+    for (i = 0; i < employeeList.length; i++) 
+    {
+
+      var name = employeeList[i].name;
+      var officeNum = employeeList[i].officeNum;
+      var phoneNum = employeeList[i].phoneNum;
+
+      $('.content').append(theCard(name, officeNum, phoneNum));
+
     }
   }
-  render();
+
+  //2. Render all the names in employee list when view button is pressed
+
+  const view = function()
+  {
+      render();
+  }
+
+//   3. An `Add` option that allows users to input name, office number, and phone number and then renders the updated employee list.
+  
+  const add = function()
+  {
+      const addName = $('#name').val();
+      const addOfficeNum = $('#officeNumber').val();
+      const addPhoneNum = $('#officeNumber').val();
+      employeeList.push({"name":addName,"officeNum":addOfficeNum,"phoneNum":addPhoneNum});
+
+      render();
+
+      //Clear the fields and remove class
+      $('#showContent').removeClass('d-none');
+  }
+
+//   4. A `Verify` option that allows users to input a name and renders `yes` if the employee exists and `no` otherwise.
+
+const verify = function()
+{
+  const searchName = $('#nameInput').val();
+  const index = employeeList.findIndex((obj => obj.name == searchName));
+  $('.content').empty();
+  if (index > -1) {
+    $('.content').append(`<p>Yes</p>`);
+  } else {
+    $('.content').append(`<p>No</p>`);
+  }
+  $('#showContent').removeClass('d-none');
+}
 
 
+
+$('#view').on('click', view);
+
+// 4. An `Update` option that allows the user to input name, office number, and phone number and updates the office number and phone number of the employee that matches the input name, and then renders the updated employee list.
+
+
+//5. A `Delete` option that deletes the employee with the matching name and then renders the updated employee list.
